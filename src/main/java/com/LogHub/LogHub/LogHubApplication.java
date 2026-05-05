e8b38863d3477eb3cc31c225b8d6d0d4a957c1b4
nazareno11 <nazarenoluna86@gmail.com>
2026-05-05 10:10:36 -0300

    1er commit

---
+++ src/main/java/com/LogHub/LogHub/LogHubApplication.java
@@ -0,0 +1,13 @@
+package com.LogHub.LogHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LogHub/LogHub/LogHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LogHub.LogHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
