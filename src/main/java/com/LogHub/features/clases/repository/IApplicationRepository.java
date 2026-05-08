9d48eafdd27455d9253d0975f0cd55bd3d3d0e39
nazareno11 <nazarenoluna86@gmail.com>
2026-05-08 09:26:22 -0300

    fix error

--- src/main/java/com/LogHub/features/clases/repository/IApplicationRepository.java
+++ src/main/java/com/LogHub/features/clases/repository/IApplicationRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.LogHub.features.clases.entity.Application;
 
 public interface IApplicationRepository extends JpaRepository <Application, Long> {
-    Optional <Application> findByApikey (UUID apikey);
+    Optional<Application> findByApiKey(UUID apiKey);
     boolean existsByEmail (String email);
 }
