46e3b885c907ee5c33abded4b9111174b92ace0b
nazareno11 <nazarenoluna86@gmail.com>
2026-05-11 20:17:16 -0300

    Fix LogRepository

--- src/main/java/com/LogHub/features/clases/repository/ILogRepository.java
+++ src/main/java/com/LogHub/features/clases/repository/ILogRepository.java
@@ -1,5 +1,23 @@
 package com.LogHub.features.clases.repository;
 
-public class ILogRepository {
+import java.time.LocalDateTime;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.LogHub.features.clases.entity.Log;
+
+public interface ILogRepository extends JpaRepository<Log, Long>{
+    /*Filtrar por application */
+    List<Log> findByApplicationId(Long applicationId);
+
+    /*Filtro por fechas */
+    List<Log> findByTimestampBetween(LocalDateTime from, LocalDateTime to);
+
+    /*Filtrar por application y fechas */
+    List<Log> findByApplicationIdAndTimestampBetween(
+            Long applicationId,
+            LocalDateTime from,
+            LocalDateTime to
+    );
 }
