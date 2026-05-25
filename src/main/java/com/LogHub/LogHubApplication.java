1d6838e7bfe03706a4d163f11d0a3b83fd4633ab
Nazareno Luna <nazarenoluna86@gmail.com>
2026-05-25 19:20:55 -0300

    Fix VE

--- src/main/java/com/LogHub/LogHubApplication.java
+++ src/main/java/com/LogHub/LogHubApplication.java
@@ -3,17 +3,26 @@ package com.LogHub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.LogHub.config.EnvLoader;
+
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class LogHubApplication {
-	
-	static {
-        new EnvLoader();
+
+    public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./LogHub")
+                .ignoreIfMalformed()
+                .ignoreIfMissing()
+                .load();
+
+
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+        SpringApplication.run(LogHubApplication.class, args);
     }
-
-	public static void main(String[] args) {
-		SpringApplication.run(LogHubApplication.class, args);
-	}
-
 }
